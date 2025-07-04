8aecfc3130ce7a4480147334eab4aaf0c8819336
Craig P. Motlin <cmotlin@gmail.com>
2025-07-03 20:11:42 -0400

    Fix formatting consistency and add iOS build files.

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,8 +1,7 @@
-/* eslint-env node */
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
-  };
+module.exports = function (api) {
+	api.cache(true);
+	return {
+		presets: ['babel-preset-expo'],
+		plugins: ['react-native-reanimated/plugin'],
+	};
 };
--- metro.config.cjs
+++ metro.config.cjs
@@ -1,6 +1,4 @@
-/* eslint-env node */
-
-const { getDefaultConfig } = require('expo/metro-config');
+const {getDefaultConfig} = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
